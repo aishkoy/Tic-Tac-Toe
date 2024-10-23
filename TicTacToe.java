970a87c6d79e841f44d06fe40b2728215e7870ed
aishk <aisha.orozbekova@alatoo.edu.kg>
2024-10-23 17:29:52 +0600

    added game field

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,5 +4,23 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[][] field = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = 0;
+            }
+        }
+
+        showEmptyField(field);
+    }
+
+    public static void showEmptyField(int[][] field) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("⬜ ");
+            }
+            System.out.println();
+        }
     }
 }
