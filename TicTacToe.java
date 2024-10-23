e2c80e9d966e01e055338ded27b2ade61887fbd7
aishk <aisha.orozbekova@alatoo.edu.kg>
2024-10-23 17:39:44 +0600

    added players class

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TicTacToe {
+    static ArrayList<Player> players = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -10,6 +13,32 @@ public class TicTacToe {
             for (int j = 0; j < 3; j++) {
                 field[i][j] = 0;
             }
+        }
+
+        System.out.print("Player 1, enter your name: ");
+        String name = sc.nextLine();
+        while(name.isBlank()){
+            System.out.print("You didn't enter name. Try again: ");
+            name = sc.nextLine();
+        }
+        Player player = getPlayer(name);
+
+        if(player == null){
+            player = new Player(name);
+            players.add(player);
+        }
+
+        System.out.print("Player 2, enter your name: ");
+        String name2 = sc.nextLine();
+        while(name2.isBlank()){
+            System.out.print("You didn't enter name. Try again: ");
+            name2 = sc.nextLine();
+        }
+        Player player2 = getPlayer(name2);
+
+        if(player2 == null){
+            player2 = new Player(name2);
+            players.add(player2);
         }
 
         showHintField();
@@ -39,5 +68,68 @@ public class TicTacToe {
         }
     }
 
+    public static Player getPlayer(String name) {
+        for (Player p : players) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
 
+    static class Player {
+        private String name;
+        private int wins;
+        private int losses;
+
+        public Player(String name) {
+            this.name = name;
+            this.wins = 0;
+            this.losses = 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getWins() {
+            return wins;
+        }
+
+        public int getLosses() {
+            return losses;
+        }
+
+        public void incrementWins() {
+            wins++;
+        }
+
+        public void incrementLosses() {
+            losses++;
+        }
+    }
+
+    public static void sortPlayersByWins(Player[] players) {
+        int n = players.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (players[j].getWins() < players[j + 1].getWins()) {
+                    Player temp = players[j];
+                    players[j] = players[j + 1];
+                    players[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void displayLeaderboard() {
+        Player[] playerArray = new Player[players.size()];
+        players.toArray(playerArray);
+
+        sortPlayersByWins(playerArray);
+
+        for (Player p : playerArray) {
+            System.out.println(p.getName() + " - Wins: " + p.getWins() + ", Losses: " + p.getLosses());
+        }
+    }
 }
